24eddb30484987442ee83ac321d242048b6555ca
Christian Jensen <>
2019-11-26 15:35:54 -0600

    added method functionality for write/read users

--- web/registration/registration.js
+++ web/registration/registration.js
@@ -1,27 +1,26 @@
-function loadJSON(path, callback) {   
+let users = [];
 
-    let xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', path, true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
+test = () => {
+    console.log(users);
+    let newUser = {
+        "username": document.getElementById('username').value,
+        "password": document.getElementById('password').value,
+        "firstName": document.getElementById('firstname').value,
+        "lastName": document.getElementById('lastname').value
+    }
 
- function test() { 
-    let json;
-    loadJSON("../../data/db.json", function(response) {
-        json = JSON.parse(response);
-        // Call another function with json that is now filled with data
-        triggered(json);
-    });
+    saveToLocalStorage(newUser);
 }
 
-function triggered(json) {
-    console.log(json[0]);
-    // Do your work on json
+saveToLocalStorage = (user) => {
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+    console.log(users);
+}
+
+window.onload = () => {
+    const admin = { username: 'admin', password: 'admin', firstName: 'admin', lastName: 'user' };
+    users.push(admin);
+    localStorage.setItem('users', JSON.stringify(users));
+    users = JSON.parse(localStorage.getItem("users"));
 }
