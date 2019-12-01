9f26d345bd6d5c4afe3c20e249a36319daf3975a
Christian Jensen <>
2019-12-01 14:48:54 -0600

    protecting routes

--- web/login/login.js
+++ web/login/login.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+window.onload = () => {
+    users = JSON.parse(localStorage.getItem("users"));
+
+    redirectTo();
+}
+
+redirectTo = () => {
+    let loggedOn = false;
+    users.forEach(user => {
+        if (user.loggedOn === true) {
+            loggedOn = true;
+        }
+    });
+
+    if (loggedOn === true) {
+        window.open('../home/home.html', '_self');
+    }
+}
--- web/registration/registration.js
+++ web/registration/registration.js
@@ -2,6 +2,21 @@ let users = [];
 
 window.onload = () => {
     users = JSON.parse(localStorage.getItem("users"));
+
+    redirectTo();
+}
+
+redirectTo = () => {
+    let loggedOn = false;
+    users.forEach(user => {
+        if (user.loggedOn === true) {
+            loggedOn = true;
+        }
+    });
+
+    if (loggedOn === true) {
+        window.open('../home/home.html', '_self');
+    }
 }
 
 createUser = () => {
