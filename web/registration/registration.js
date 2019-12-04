4c6f3c335bb079c6cc374d1047b26a8f0e97d332
JunyuTao <jtk54@mail.umsl.edu>
2019-12-04 16:49:14 -0600

    1

---
+++ web/registration/registration.js
@@ -0,0 +1,37 @@
+var username = document.getElementById("username")
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var password2 = document.getElementById("password2")
+var phonenumber = document.getElementById("phonenumber")
+
+
+
+function newElement()
+{
+    if(username == "")
+    {
+        alert("Please fill all blanks.")
+        
+    }
+
+    if(email == "")
+    {
+        alert("Please fill all blanks.") 
+    }
+
+    if(password == "")
+    {
+        alert("Please fill all blanks.")
+    }
+
+    if(password2 =="")
+    {
+        alert("Please fill all blanks.")    
+    }
+
+    if(phonenumber == "")
+    {
+        alert("Please fill all blanks.")
+    }
+}
+
